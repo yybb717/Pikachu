d79461a25bb16ee1b2a6e219ecd987d582c33dbb
yybb717 <2638143715@qq.com>
2020-03-14 21:13:16 +0800

    一键部署

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 yarn build &&  
 git checkout gh-pages &&
-rm -rf *.htm *.js *.css *.png &&   
+rm -rf *.html *.js *.css *.png &&   
+mv dist/* ./ &&
 rm -rf dist  ;
 git add . &&
 git commit -m "update" &&
